4d7bb2fd53d2496befa8fe0be74fc67b932aadcc
Rafi Qadri <rqadri133@users.noreply.github.com>
2016-08-13 18:32:01 -0700

    Changes  Needed
    Changed Needed

---
+++ services.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+
+app.service("OMSService", function ($http)  
+  {  
+                this.getCustomers = function () {
+                return $http.get("api/Customers")
+
+              }
+
+    
+              this.saveCustomer = function (customerInfo) {
+                  return $http(
+                    {
+                        method: 'post',
+                        data: customerInfo,
+                        url: 'api/Customers'
+
+
+                    });
+              }
+
+
+});
+
+     
